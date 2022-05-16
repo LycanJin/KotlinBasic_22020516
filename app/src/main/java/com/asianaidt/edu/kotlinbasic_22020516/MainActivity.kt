78fef75097bec88b600aeb9803274a293854ec7c
Jin <lycanjin@gmail.com>
2022-05-16 14:20:27 +0900

    Jin 1. 로그 출력 하도록 구현 실습

--- app/src/main/java/com/asianaidt/edu/kotlinbasic_22020516/MainActivity.kt
+++ app/src/main/java/com/asianaidt/edu/kotlinbasic_22020516/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.asianaidt.edu.kotlinbasic_22020516
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btn_log.setOnClickListener {
 
+            // btn_log 가 클릭되면 들어옴
+            Log.d("메인화면", "로그 찍기 버튼 눌림")
+        }
     }
 }
